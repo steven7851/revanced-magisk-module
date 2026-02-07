9205e2a2b43148d8f7a6a88d449983cec1c785a5
steven7851 <steven7851@msn.com>
2026-02-07 12:34:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -124,7 +124,13 @@ for table_name in $(toml_get_table_names); do
 		app_args[archive_dlurl]=${app_args[archive_dlurl]%/}
 		app_args[dl_from]=archive
 	} || app_args[archive_dlurl]=""
-	if [ -z "${app_args[dl_from]-}" ]; then abort "ERROR: no 'apkmirror_dlurl', 'uptodown_dlurl' or 'archive_dlurl' option was set for '$table_name'."; fi
+
+	# Added GitHub Release parsing
+	app_args[github_release_dlurl]=$(toml_get "$t" github-release-dlurl) && {
+		app_args[dl_from]=github_release
+	} || app_args[github_release_dlurl]=""
+
+	if [ -z "${app_args[dl_from]-}" ]; then abort "ERROR: no 'github_release_dlurl', 'apkmirror_dlurl', 'uptodown_dlurl' or 'archive_dlurl' option was set for '$table_name'."; fi
 	app_args[arch]=$(toml_get "$t" arch) || app_args[arch]="all"
 	if [ "${app_args[arch]}" != "both" ] && [ "${app_args[arch]}" != "all" ] && [[ ${app_args[arch]} != "arm64-v8a"* ]] && [[ ${app_args[arch]} != "arm-v7a"* ]]; then
 		abort "wrong arch '${app_args[arch]}' for '$table_name'"
