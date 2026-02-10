78655fc3be9b324c9c5ae1c1210fe3910379026e
steven7851 <steven7851@msn.com>
2026-02-11 00:08:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -85,7 +85,7 @@ for table_name in $(toml_get_table_names); do
 	app_args[cli]=$cli_jar
 	app_args[ptjar]=$patches_jar
 	if [[ -v cliriplib[${app_args[cli]}] ]]; then app_args[riplib]=${cliriplib[${app_args[cli]}]}; else
-		if [[ $(java -jar "${app_args[cli]}" patch 2>&1) == *rip-lib* ]]; then
+		if [[ $(java -jar "${app_args[cli]}" patch 2>&1) == *striplibs* ]]; then
 			cliriplib[${app_args[cli]}]=true
 			app_args[riplib]=true
 		else
