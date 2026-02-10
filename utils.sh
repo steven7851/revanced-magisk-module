93001f1ab0e315782f145b752a5a9ff46a38eaed
steven7851 <steven7851@msn.com>
2026-02-10 23:54:26 +0800

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -94,7 +94,11 @@ get_prebuilts() {
 			if [ "$(jq 'length' <<<"$matches")" -ne 1 ]; then
 				epr "More than 1 asset was found for this cli release. Fallbacking to the first one found..."
 			fi
-			asset=$(jq -r ".[1]" <<<"$matches")
+			if [ "$tag" = "CLI" ]; then
+				asset=$(jq -r ".[1]" <<<"$matches")
+			else
+				asset=$(jq -r ".[0]" <<<"$matches")
+			fi
 			url=$(jq -r .url <<<"$asset")
 			name=$(jq -r .name <<<"$asset")
 			file="${dir}/${name}"
